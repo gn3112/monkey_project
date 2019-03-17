38ff9d668387a708209d779cd4c16de0caa882b3
Georges Nomicos <georgesnomicos@gmail.com>
2019-03-17 16:45:35 +0000

    added validation

--- pytorch_nn.py
+++ pytorch_nn.py
@@ -13,31 +13,51 @@ class Network(nn.Module):
         super(Network,self).__init__()
         self.fc1 = nn.Linear(98,size_layer)
         self.fc2 = nn.Linear(size_layer,size_layer)
-        self.fc3 = nn.Linear(size_layer,3)
+        self.fc3 = nn.Linear(size_layer,size_layer)
+        self.fc4 = nn.Linear(size_layer,3)
 
     def forward(self,x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         # x = F.tanh(self.fc3(x)) * 120
-        x = self.fc3(x)
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
         return x
-def main():
-    n_epoch = 20
-    batch_size = 256
-    window_size = 25000/batch_size * n_epoch + 300
-    size_layer = 512
 
+def data_loader(batch_size):
     data_features = pd.read_csv('monkey_features.csv')
     data_targets = pd.read_csv('monkey_targets.csv')
+    trial_idx = data_targets.iloc[:,4].as_matrix()
+    move_idx = data_targets.iloc[:,:5].as_matrix()
     data_targets = data_targets.iloc[:,:3].as_matrix()
     data_features = data_features.iloc[:,:].as_matrix()
-    train = TensorDataset(torch.tensor(data_features).float(), torch.tensor(data_targets).float())
-    train_loader = DataLoader(train, batch_size=batch_size, shuffle=True)
+    for idx in range(len(trial_idx)):
+        if trial_idx[idx] == 700:
+            break
+
+    train = TensorDataset(torch.tensor(data_features[:idx,:]).float(), torch.tensor(data_targets[:idx,:]).float())
+    train_loader = DataLoader(train, batch_size=batch_size, shuffle=True, num_workers=4)
+
+    valid = TensorDataset(torch.tensor(data_features[idx:,:]).float(), torch.tensor(data_targets[idx:,:]).float())
+    valid_loader = DataLoader(valid, batch_size=batch_size, shuffle=True, num_workers=4)
+
+    return train_loader, valid_loader
+
+
+def main():
+    n_epoch = 60
+    batch_size = 256
+    window_size = 25362/batch_size * n_epoch + 200
+    size_layer = 512
+
+    train_loader, valid_loader = data_loader(batch_size)
 
     network = Network(size_layer)
-    optimiser = optim.Adam(network.parameters(), lr=1e-2, betas=(0.5, 0.999))
+    optimiser = optim.Adam(network.parameters(), lr=7e-3, betas=(0.5, 0.999))
 
     log_loss = []
+    log_loss_val = []
+    log_it_val = []
     for epoch in range(n_epoch):
         for batch_idx, (neuron, position) in enumerate(train_loader):
             optimiser.zero_grad()
@@ -49,9 +69,19 @@ def main():
 
             if batch_idx % 50 == 0 and batch_idx !=0:
                 print('----- Epoch: ' + str(epoch+1) + ' Iteration: ' + str(batch_idx) + ' Loss: ' + str(log_loss[-1]) + '-----')
+        loss_val = 0
+        for batch_idx, (neuron, position) in enumerate(valid_loader):
+            network.eval()
+            output_v = network(neuron)
+            loss_val += F.l1_loss(output_v, position)
+        loss_val /= (batch_idx+1)
+        log_loss_val.append(loss_val.detach().numpy())
+        log_it_val.append((epoch+1)*25362/batch_size)
+
         # plt.figure()
         plt.axis([0,window_size,0,25])
         plt.plot(log_loss,color='b')
+        plt.plot(log_it_val,log_loss_val,color='r')
         plt.draw()
         plt.pause(0.01)
 
